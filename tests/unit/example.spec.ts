7670b2e2c3498acc168bbe8bc612fd4c316748fd
Nicolas <nicolasdelcourt10@gmail.com>
2022-06-16 11:30:39 +0200

    First commit

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,14 @@
+import { screen, render } from "@testing-library/vue";
+import "@testing-library/jest-dom";
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import "@testing-library/jest-dom";
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+
+    render(HelloWorld, { props: { msg } });
+    expect(screen.getByText(msg)).toEqual(msg);
+    expect(screen.getByText("Installed CLI Plugins")).toBeInTheDocument();
   });
 });
